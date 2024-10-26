89cb340982ece716057e68265a6dc2f846cd7e73
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-27 00:39:15 +0530

    Update datatypes.js

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -5,3 +5,18 @@ let id = 7
 console.log(typeof(id))
 // console.log(typeof null) null type is object 
 // console.log(typeof undefined) undefined type is undefined
+
+// return type using typeof function
+// Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object

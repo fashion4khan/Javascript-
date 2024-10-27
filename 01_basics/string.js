624e5b314440913dd8c8f6ee7500ccf4e876211f
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-27 15:49:03 +0000

    container config done

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+const name = "     fashion khan"
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(3))
+// console.log(name.indexOf("n"))
+// console.log(name.substring(0,7))
+// console.log(name.slice(0,4))
+// console.log(name.trim())
+
+const gamename = new String("ludo-king")

c926436e43e8cc60b2a4b3a5bf85c7a0e67bf70a
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-28 06:49:38 +0000

    container config done

---
+++ 01_basics/dateAndTime.js
@@ -0,0 +1,8 @@
+let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toJSON())
+
+let myStamp = Date.now()
+console.log(myStamp)

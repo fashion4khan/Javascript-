4766b82fd24cd015f3c5290a0b5c6ece714377dc
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-28 05:49:10 +0000

    container config done

---
+++ 01_basics/numberAndMath.js
@@ -0,0 +1,19 @@
+const num = 100
+// console.log(num)
+
+const myNum = new Number(100)
+
+// console.log(myNum.toFixed(1))
+
+
+// const hundred = 10000000;
+// console.log(hundred.toLocaleString('en-IN'))
+
+// console.log(Math.abs(-4))
+// console.log(Math.floor(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.round(4.76))
+// console.log(Math.min(2, 3, 4, 1))
+// console.log(Math.max(2, 3, 4, 1))
+
+console.log(Math.floor((Math.random() * 10)+1))

e0e4f257697bc6f4c6f1187730032dcc8b37744a
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-30 20:42:15 +0000

    conatiner config done

---
+++ 03_basics/arrow.js
@@ -0,0 +1,47 @@
+// const user = {
+//     username : "hitesh",
+//     price : 999,
+
+//     WelcomeMsg : function()
+//     {
+//         console.log(`${this.username} , welcome to website !`)
+//         console.log(this)
+//     }
+// }
+// user.WelcomeMsg()
+
+// function chai() {
+//     let username = "hitesh"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+// arrow function
+
+// const chai = () => {
+// let username = "hitesh"
+// console.log(this.username);
+// } 
+// chai()
+
+// const addTwoNum = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(addTwoNum(4,3))
+
+// const addTwoNum = (num1,num2) => num1 + num2
+
+// console.log(addTwoNum(4, 2))
+
+// const addTwoNum = (num1,num2) => (num1 + num2)
+
+// console.log(addTwoNum(4,3))
+
+// const addTwoNum = (num1,num2) => {return num1 + num2} 
+
+// console.log(addTwoNum(4, 2))

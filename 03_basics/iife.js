53fcf3a4f1db57462c13687cc96b83e32814f93e
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-10-30 20:55:45 +0000

    container config done

---
+++ 03_basics/iife.js
@@ -0,0 +1,30 @@
+// NORMAL FUNCTION
+
+// function chai()
+// {
+//     console.log("welcome !")
+// }
+// chai()
+
+// IIFE IS USED TO IMMEDIATE RUN TO FUNCTION AND AVOID POLLOUTION OF GLOBAL SCOPE
+
+// (function chai()
+// {
+//     console.log("Welcome again !")
+// }
+// )()
+
+// USING ARROW FUNCTION WITHOUT PASSING PARAMETES
+
+// ( () => 
+// {
+//     console.log("Welcome to the code practice !")
+// }
+// )()
+
+// USING ARROW FUNCTION WITH PASSING PARAMETES
+
+// ( (name) =>
+// {
+//     console.log(`${name} is a good boy !`)
+// })("fashion khan")
